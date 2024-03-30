6342ad58698f5efbc9e72981436b369b39f36c25
CLAUDIO LUCAS <claudio.lucas31@gmail.com>
2024-03-29 22:08:50 -0300

    Update workspace.xml

--- main.py
+++ main.py
@@ -8,13 +8,8 @@ from kivymd.uix.screen import MDScreen
 Window.size = (350, 580)
 
 
-class MDExtendedFabButton:
+class SegScreen(MDScreen):
     pass
-
-
-class SegScreen(MDScreen, MDExtendedFabButton):
-    pass
-
 
 
 class MainApp(MDApp):
@@ -23,15 +18,17 @@ class MainApp(MDApp):
         screen_manager.add_widget(Builder.load_file("tela.kv"))
         screen_manager.add_widget(Builder.load_file("segunda.kv"))
 
-
-
         return screen_manager
 
     def fechar(self):
         self.stop()
 
+
+
 if __name__ == "__main__":
-    LabelBase.register(name="MPoppins", fn_regular="C:\\Users\\ARTHUR GAMER4\\PycharmProjects\\aplicativocomercial\\Poppins\\Poppins-Medium.ttf")
-    LabelBase.register(name="BPoppins", fn_regular="C:\\Users\ARTHUR GAMER4\\PycharmProjects\\aplicativocomercial\\Poppins\\Poppins-SemiBold.ttf")
+    LabelBase.register(name="MPoppins",
+                       fn_regular="C:\\Users\\ARTHUR GAMER4\\PycharmProjects\\aplicativocomercial\\Poppins\\Poppins-Medium.ttf")
+    LabelBase.register(name="BPoppins",
+                       fn_regular="C:\\Users\ARTHUR GAMER4\\PycharmProjects\\aplicativocomercial\\Poppins\\Poppins-SemiBold.ttf")
 
 MainApp().run()
